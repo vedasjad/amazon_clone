3e0849695b8f99a2343ae68d426e9181343adfbb
vedasjad <vedasjad@gmail.com>
2023-02-05 10:42:21 +0530

    UserProvider Made

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
-
 import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:amazon_clone/models/user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../../constants/global_variables.dart';
+import '../../../providers/user_provider.dart';
 
 class AuthService {
   void signUpUser({
@@ -77,15 +80,86 @@ class AuthService {
       debugPrint(res.body);
 
       httpErrorHandle(
-          response: res,
-          context: context,
-          onSuccess: () {
-            // showSnackBar(
-            //   context,
-            //   '',
-            // );
-          });
+        response: res,
+        context: context,
+        onSuccess: () async {
+          if (keepSignedIn) {
+            final prefs = await SharedPreferences.getInstance();
+            Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+            await prefs.setString(
+                'x-auth-token', jsonDecode(res.body)['token']);
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              HomeScreen.routeName,
+              (route) => false,
+            );
+          }
+        },
+      );
       debugPrint("Checkpoint 3");
+    } catch (e) {
+      debugPrint("Checkpoint 4");
+      debugPrint(e.toString());
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  void getUserData({
+    required BuildContext context,
+  }) async {
+    try {
+      debugPrint("Checkpoint 1");
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      var tokenRes = await http.post(
+        Uri.parse('$uri/tokenIsValid'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token!,
+        },
+      );
+
+      var response = jsonDecode(tokenRes.body);
+
+      if (response == true) {
+        http.Response userRes = await http.get(
+          Uri.parse('$uri/'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': token,
+          },
+        );
+
+        var userProvider = Provider.of<UserProvider>(context, listen: false);
+        userProvider.setUser(userRes.body);
+      }
+
+      // debugPrint(res.body);
+      //
+      // httpErrorHandle(
+      //   response: res,
+      //   context: context,
+      //   onSuccess: () async {
+      //     if (keepSignedIn) {
+      //       final prefs = await SharedPreferences.getInstance();
+      //       Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+      //       await prefs.setString(
+      //           'x-auth-token', jsonDecode(res.body)['token']);
+      //       Navigator.pushNamedAndRemoveUntil(
+      //         context,
+      //         HomeScreen.routeName,
+      //             (route) => false,
+      //       );
+      //     }
+      //   },
+      // );
+      // debugPrint("Checkpoint 3");
     } catch (e) {
       debugPrint("Checkpoint 4");
       debugPrint(e.toString());
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +55,9 @@ class MyApp extends StatelessWidget {
         ),
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? const HomeScreen()
+          : const AuthScreen(),
     );
   }
 }
