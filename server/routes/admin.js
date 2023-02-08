d288a43f8c6dec81a01bc4216ecb45193d5a1b60
vedasjad <vedasjad@gmail.com>
2023-02-08 14:53:42 +0530

    Delete Products Error resolved

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -35,7 +35,6 @@ adminRouter.post('/admin/delete-product',admin,async(req,res)=>{
   try{
     const {id} = req.body;
     let product = await Product.findByIdAndDelete(id);
-    product = await product.save();
     res.json(product);
   }catch(e){
     res.status(500).json({error: e.message});
