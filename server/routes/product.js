7625edd63244955dca65e82dab5b488c5d90a3ed
vedasjad <vedasjad@gmail.com>
2023-02-08 16:20:14 +0530

    Fetch By Category Error resolved

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const productRouter = express.Router();
-const auth = require('../middlewares/auth');
 const Product = require('../models/product');
+const admin = require('../middlewares/admin');
 
 productRouter.get("/api/products",admin,async(req,res)=>{
   try{
